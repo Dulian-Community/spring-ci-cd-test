783b3ea724ac6137eccef122ccf6ba54d0ba6826
Woong99 <dndry1072@naver.com>
2024-12-30 22:20:30 +0900

    test

---
+++ src/test/kotlin/dulian/dulian/domain/auth/service/CITest.kt
@@ -0,0 +1,13 @@
+package dulian.dulian.domain.auth.service
+
+import io.kotest.core.spec.style.DescribeSpec
+
+class CITest:DescribeSpec({
+
+    describe("CI 테스트") {
+        it("CI 테스트") {
+            println("CI 테스트")
+        }
+    }
+}) {
+}
